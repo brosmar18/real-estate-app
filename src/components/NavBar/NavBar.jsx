53c532392416ac7a2b9d332c4d482bbcf0b434c7
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-22 22:30:01 -0500

    responsive navbar added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { navLinks } from "../../constants";
 import { NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 import "./NavBar.scss";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="nav">
       <div className="logo">
@@ -24,6 +27,14 @@ const NavBar = () => {
       <a href="/" className="register">
         Sign In
       </a>
+      <div className="hamburger">
+        <FiMenu size={30} onClick={() => setOpen(!open)} />
+      </div>
+      <div className={open ? "menu active" : "menu"}>
+        {navLinks.map((link, index) => (
+          <NavLink key={`nav-links-${index}`}>{link.label}</NavLink>
+        ))}
+      </div>
     </nav>
   );
 };
